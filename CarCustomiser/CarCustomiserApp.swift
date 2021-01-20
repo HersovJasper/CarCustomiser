d26853df8ff59f83524b0e9c4effa70f3d33b45d
Jasper Hersov <Hersov.J@etoncollege.org.uk>
2021-01-20 11:00:07 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Jasper Hersov on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
